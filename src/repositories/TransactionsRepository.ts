3e7a98b2987c799a0dd97d80f61f245c1b59d4ba
Flávio Pangrácio <flaviohugo.14@gmail.com>
2020-04-22 01:07:40 -0300

    Passed in all tests \o/

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,12 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -14,15 +20,39 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const outcomes = this.transactions.map(transaction =>
+      transaction.type === 'outcome' ? transaction.value : 0,
+    );
+
+    const sumOutcomes = outcomes.reduce((total, outcome) => total + outcome, 0);
+
+    const incomes = this.transactions.map(transaction =>
+      transaction.type === 'income' ? transaction.value : 0,
+    );
+
+    const sumIncomes = incomes.reduce((total, outcome) => total + outcome, 0);
+
+    const total = sumIncomes - sumOutcomes;
+
+    const balance = {
+      income: sumIncomes,
+      outcome: sumOutcomes,
+      total,
+    };
+
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
